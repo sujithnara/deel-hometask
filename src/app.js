ba0b07c54625b0a5631f699a87d2b9b3bedaafe4
Sujith Nara <sujith.nara@smarsh.com>
2025-02-26 14:09:37 +0530

    2. Returns a list of non-terminated contracts belonging to a user

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
+const {Op} = require('sequelize')
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
@@ -23,6 +24,26 @@ app.get('/contracts/:id',getProfile ,async (req, res) =>{
     }
 
     res.json(contract)
+})
+
+/**
+ * @returns list of contracts belonging to a user (client or contractor) and non-terminated
+ */
+app.get('/contracts', getProfile, async (req, res) => {
+    const {Contract} = req.app.get('models')
+    const profileId = req.profile.id
+    const contracts = await Contract.findAll({
+        where: {
+            [Op.or]: [
+                {ClientId: profileId},
+                {ContractorId: profileId}
+            ],
+            status: {
+                [Op.ne]: 'terminated'
+            }
+        }
+    })
+    res.json(contracts)
 })
 
 module.exports = app;
